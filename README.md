b437fc400c0900a9fc8201b14f4e128f87d43422
Sami <h4ck4n0nyt@gmail.com>
2021-08-10 00:44:34 +0300

    done

---
+++ README.md
@@ -0,0 +1,15 @@
+# Random Word and Action Generater 
+This was a project given by codecademy fullstack developer path. The purpose of this program is to generate a random word with a random action to perform. The words and the actions are in the same object but different elements as an array.
+## Tools
+* NodeJsjs for running the JavaScript code
+* Visual Studio Code
+## Requirments 
+* NodeJs, you can install it from https://nodejs.org/
+## Usage
+1. Clone the repository
+    `git clone https://github.com/h4ck4n0n/fortuneTeller.git`
+2. Open your terminal and type
+    `node script.js`
+## Credits
+1. The source for the actions https://www.bestrandoms.com/random-task
+2. Codecademy 
--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+//Random number generator
+function randNumGen(list){
+    return Math.floor(Math.random() * list)
+}
+let choices = {
+    words: ['flexibility','adventure','bravery','charisma','independence','determination','paitence','vibrancy','romance','inspiration','dedication','liveliness'],
+    actions: ['Learn all About Home Remedies','Try 50 Different Kinds of Beer','Work As a Counselor','Share a Pet','Trek to Everest Base Camp','Eat at a 5 Star Restaurant','Eat Something Foreign That Looks Disgusting','Build A Community of Like-Minded People', 'Go Hiking', 'Get a Hug from a Monkey','Put your Mouth Under an Ice Cream Machine','Join the Civil Air Patrol','Cross-Country Road Trip With Friends','Finish the 60 Day Insanity Workout','Dye your Hair a Bright Color','Have Afternoon Tea in an Expensive Hotel','Hit a Hole in One']
+}
+//random word and action generator
+function generate(){
+    return `The random word for today is \'${choices.words[randNumGen(choices.words.length - 1)]}\' and the random action to do is, ${choices.actions[randNumGen(choices.actions.length - 1)]}`
+}
+console.log(generate())
